9756af12e69222c53644d584fb473d763b2948d4
sunyuc24 <690510390@qq.com>
2019-08-12 12:16:34 +0800

    LintCode 9. Fizz Buzz

--- 9. Fizz Buzz.cpp
+++ 9. Fizz Buzz.cpp
@@ -3,43 +3,45 @@ public:
     /**
      * @param n: An integer
      * @return: A list of strings.
-     */
-    vector<string> fizzBuzz(int n) {
-        // write your code here
+     */     
+	vector<string> fizzBuzz(int n) {
         vector<string> vstr;
-	    string str;
-	    for (int i = 1; i < n + 1; i++){
-		    while (!(i % 3) || !(i % 5)){
-			    while (!(i % 15)){
-				    vstr.push_back("fizz buzz");
-				    i += 1;
-				    break;
-			    }
-			    while (!(i % 3)){
-				    vstr.push_back("fizz");
-				    i += 1;
-				    break;
-			    }
-			    while (!(i % 5)){
-				    vstr.push_back("buzz");
-				    i += 1;
-				    break;
-			    }
-		    }
-		str = to_string(i);
-		vstr.push_back(str);
-	}
-	while (vstr.size() > n){
-		vstr.pop_back();
-	}
-	return vstr;
-    }
-                  /*ANOTHER WAY*/
- //    vector<string> fizzBuzz(int n) {
- //        vector<string> vstr;
-	// 	for (int i = 1; i < n + 1; i++){
-	// 	    i % 15 ? (i % 3 ? (i % 5 ? vstr.push_back(to_string(i)) : vstr.push_back("buzz")) : vstr.push_back("fizz")) : vstr.push_back("fizz buzz");
-	//     }
-	// return vstr;
- //    }
+	 	for (int i = 1; i < n + 1; i++){
+	 	    i % 15 ? (i % 3 ? (i % 5 ? vstr.push_back(to_string(i)) : vstr.push_back("buzz")) : vstr.push_back("fizz")) : vstr.push_back("fizz buzz");
+	     }
+	 return vstr;
+     }
+
+                /*ANOTHER WAY*/
+  //   vector<string> fizzBuzz(int n) {
+  //   	// write your code here
+  //   	vector<string> vstr;
+	 //    string str;
+	 //    for (int i = 1; i < n + 1; i++){
+		//     while (!(i % 3) || !(i % 5)){
+		// 	    while (!(i % 15)){
+		// 		    vstr.push_back("fizz buzz");
+		// 		    i += 1;
+		// 		    break;
+		// 	    }
+		// 	    while (!(i % 3)){
+		// 		    vstr.push_back("fizz");
+		// 		    i += 1;
+		// 		    break;
+		// 	    }
+		// 	    while (!(i % 5)){
+		// 		    vstr.push_back("buzz");
+		// 		    i += 1;
+		// 		    break;
+		// 	    }
+		//     }
+		// 	str = to_string(i);
+		// 	vstr.push_back(str);
+		// }
+		// while (vstr.size() > n){
+		// 	vstr.pop_back();
+		// }
+		// return vstr;
+  //   }
+
 };
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # LintCode
-Report my LintCode problem's solution.
+Report my LintCode problem's solution.¡¢
+#########Update 2019.8.9###########
+6.Merge Two Sorted Array
+Merge two given sorted ascending integer array A and B into a new sorted integer array.
+Example:
+
+Input:  A=[1,2,3,4], B=[2,4,5,6]
+Output: [1,2,2,3,4,4,5,6]	
+Explanation: return array merged.
+This problem can solved with Binary search algorithm.
+Binary search is a fast search algorithm with run-time complexity of ¦¯(log n). 
+The array should be in the sorted form.
+Procedure binary_search
+
+   A ¡û sorted array
+   n ¡û size of array
+   x ¡û value to be searched
+
+   Set lowerBound = 1
+   Set upperBound = n 
+
+   while x not found
+      if upperBound < lowerBound 
+         EXIT: x does not exists.
+   
+      set midPoint = lowerBound + ( upperBound - lowerBound ) / 2
+      
+      if A[midPoint] < x
+         set lowerBound = midPoint + 1
+         
+      if A[midPoint] > x
+         set upperBound = midPoint - 1 
+
+      if A[midPoint] = x 
+         EXIT: x found at location midPoint
+   end while
+   
+end procedure
