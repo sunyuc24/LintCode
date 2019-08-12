351b82dc5729a13d9c36e48dc0f687fcc8e012c7
sunyuc24 <690510390@qq.com>
2019-08-12 15:45:28 +0800

    1. A + B Problem

--- 1. A + B Problem.cpp
+++ 1. A + B Problem.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-using namespace std;
-void main(){
-}
+class Solution {
+public:
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b 
+     */
+    int aplusb(int a, int b) {
+        // write your code here
+            while (b != 0){
+                int temp = a ^ b;
+                b = (a & b) << 1;
+                a = temp;
+            }
+
+        return a;
+    }
+};
